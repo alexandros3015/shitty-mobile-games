73f25e33eb195e8b264f398fd3e315b36c6b3943
alexandros3015 <alexandrosbeachtoys@gmail.com>
2025-03-13 19:49:56 -0400

    fixed movement of guy and follow. Added a non-functional floor

--- scripts/follow.gd
+++ scripts/follow.gd
@@ -1,7 +1,7 @@
-extends CharacterBody3D
+extends Node3D
 class_name FollowShpere
 
-@export var SPEED = 3.0
+@export var SPEED: float = 10.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,18 +13,12 @@ func _physics_process(delta: float) -> void:
 	var direction = Vector3.ZERO
 	
 	var input = Vector2(
-		Input.get_action_strength("right") - Input.get_action_strength("left"),
-		Input.get_action_strength("backwards") - Input.get_action_strength("forwards")
+		Input.get_action_strength("left") - Input.get_action_strength("right"),
+		Input.get_action_strength("forwards") - Input.get_action_strength("backwards")
 	)
-	
+		
 	if input.length() > 0:
 		input = input.normalized()
-		direction = (transform.basis * Vector3(input.x, 0, input.y)).normalized()
+		direction = (transform.basis * Vector3(input.x, 0, input.y)).normalized() * SPEED * delta
 	
-	if not is_on_floor():
-		velocity - get_gravity()
-	
-	velocity.x = direction.x * SPEED
-	velocity.z = direction.z * SPEED
-	
-	move_and_slide()
+	position += direction
--- scripts/guy.gd
+++ scripts/guy.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
-@export var follow_distance: float = 5
-@export var SPEED: float = 3
+@export var follow_distance: float = 2
+@export var SPEED: float = 5
 
 var follow: FollowShpere
 # Called when the node enters the scene tree for the first time.
@@ -12,14 +12,17 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	if !is_on_floor():
-		velocity -= get_gravity()
-	
+
 	if follow:
 		
+		if global_position.distance_to(follow.global_position) > follow_distance:
+			var direction = (follow.global_position - global_position).normalized()
+			velocity = direction * SPEED
+			$"Root Scene/AnimationPlayer".play("Human Armature|Run")
+		else:
+			velocity = Vector3.ZERO
+			$"Root Scene/AnimationPlayer".play("Human Armature|Idle")
 		
-			
-			
-		look_at(follow.global_position, Vector3.UP)
+		look_at(follow.global_position, Vector3.UP, true)
 
 	move_and_slide()
